b0e5e172387a09ed1f9d124f6f4e5a05c91993ea
tntyyy <tnty.working@gmail.com>
2024-03-03 21:27:18 +0300

    feat: adaptive

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -63,6 +63,7 @@ const Header = () => {
                             );
                         })}
                     </div>
+                    <BotButton className={styles.bot__mobile} />
                 </div>
             )}
         </>
