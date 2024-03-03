d1906912eb01f51d10d5e2a980229440e9e037bf
tntyyy <tnty.working@gmail.com>
2024-03-03 18:49:10 +0300

    feat: card & intro animations

--- src/layouts/Home/sections/Intro/Intro.tsx
+++ src/layouts/Home/sections/Intro/Intro.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import Image from 'next/image';
 import mushroom from '../../../../../public/assets/icons/mushroom.png';
 import coins from '../../../../../public/assets/icons/coins.png';
+import girlGif from '../../../../../public/assets/images/girl.gif';
+import boyGif from '../../../../../public/assets/images/boy.gif';
+import introBg from '../../../../../public/assets/images/introBg.png';
 import styles from './Intro.module.scss';
 
 const Intro = () => {
@@ -9,6 +12,9 @@ const Intro = () => {
         <main className={styles.intro}>
             <Image src={mushroom} alt={'mushroom'} className={styles.mushroom} />
             <Image src={coins} alt={'coins'} className={styles.coins} />
+            <Image src={girlGif} alt={'girl'} className={styles.girl} />
+            <Image src={boyGif} alt={'boy'} className={styles.boy} />
+            <Image src={introBg} alt={'intro'} className={styles.bg} />
             <div className={styles.wrapper}>
                 <h1 className={styles.title}>Beyond Gaming</h1>
                 <p className={styles.description}>A New Era of Secure, Decentralized Play</p>
--- src/layouts/Home/sections/Team/components/TeamItem.tsx
+++ src/layouts/Home/sections/Team/components/TeamItem.tsx
@@ -8,20 +8,31 @@ import twitterIcon from '../../../../../../public/assets/icons/twitterIcon.svg';
 
 const TeamItem: FC<TeamDataType> = (props) => {
     return (
-        <div className={styles.wrapper}>
-            <Image className={styles.image} src={props.icon} alt={props.name} />
-            <div className={styles.content}>
-                <div className={styles.content__top}>
-                    <span className={styles.post}>{props.post}</span>
-                    <span className={styles.name}>{props.name}</span>
+        <div className={styles.card}>
+            <div className={styles.card__inner}>
+                <div className={styles.wrapper}>
+                    <Image className={styles.image} src={props.icon} alt={props.name}/>
+                    <div className={styles.content}>
+                        <div className={styles.content__top}>
+                            <span className={styles.post}>{props.post}</span>
+                            <span className={styles.name}>{props.name}</span>
+                        </div>
+                        <div className={styles.content__footer}>
+                            <Link href={props.linkedIn} className={styles.icon}>
+                                <Image src={linkedinIcon} alt={'LinkedIn'}/>
+                            </Link>
+                            <Link href={props.twitter} className={styles.icon}>
+                                <Image src={twitterIcon} alt={'Twitter'}/>
+                            </Link>
+                        </div>
+                    </div>
                 </div>
-                <div className={styles.content__footer}>
-                    <Link href={props.linkedIn} className={styles.icon}>
-                        <Image src={linkedinIcon} alt={'LinkedIn'} />
-                    </Link>
-                    <Link href={props.twitter} className={styles.icon}>
-                        <Image src={twitterIcon} alt={'Twitter'} />
-                    </Link>
+                <div className={styles.info}>
+                    <ul className={styles.info__list}>
+                        {props.info.map((item) => {
+                            return <li key={item} className={styles.info__list__item}>{item}</li>
+                        })}
+                    </ul>
                 </div>
             </div>
         </div>
